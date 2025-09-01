caa7f315ffc83dc9b9121ee4f5df7c6f47d6efca
lexamplify <lexamplify@gmail.com>
2025-09-01 16:28:18 +0530

    removed mandarin

---
+++ src/i18n.ts
@@ -0,0 +1,13 @@
+import { createI18n } from 'vue-i18n'
+
+import en_US from './locales/en-US.json'
+
+export const i18n = createI18n({
+  legacy: false,
+  locale: 'en-US',
+  defaultLocale: 'en-US',
+  warnHtmlMessage: false,
+  messages: {
+    'en-US': en_US
+  },
+})
